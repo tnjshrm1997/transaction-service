97ade00ac0ba6d2385e433b1fb241df77da2b12e
Ashim Sharma <tnjshrm1997@gmail.com>
2021-08-31 02:08:05 +0530

    added cloud streams and few other new functionalities

--- src/main/java/com/microservices/foundation/transactionservice/controller/CustomerTransactionController.java
+++ src/main/java/com/microservices/foundation/transactionservice/controller/CustomerTransactionController.java
@@ -6,8 +6,11 @@ import com.microservices.foundation.transactionservice.service.CustomerTransacti
 import com.microservices.foundation.transactionservice.entity.CustomerAccountTransaction;
 import com.microservices.foundation.transactionservice.exception.TransactionServiceException;
 
+import com.mongodb.MongoException;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,7 +20,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/transaction-service")
+@RequestMapping("/transaction-service/transaction/")
 public class CustomerTransactionController {
     final CustomerTransactionService customerTransactionService;
 
@@ -25,7 +28,7 @@ public class CustomerTransactionController {
         this.customerTransactionService = customerTransactionService;
     }
 
-    @PostMapping("/transaction/{email}")
+    @PostMapping("{email}")
     @Retry(name = "new-transaction", fallbackMethod = "createNewTransactionFallBack")
     public ResponseEntity<String> createNewTransaction(@PathVariable String email, @RequestBody @Valid TransactionData transactionData){
         CustomerAccountTransaction customerTransaction= customerTransactionService.createCustomerTransaction(email,transactionData);
@@ -35,18 +38,22 @@ public class CustomerTransactionController {
         }
         throw new TransactionServiceException("Account Number not found");
     }
-    public ResponseEntity<String> createNewTransactionFallBack(@PathVariable String email, @RequestBody @Valid TransactionData transactionData, Exception exception){
-        throw new TransactionServiceException("Customer Service Not Available", exception);
-    }
-    @GetMapping("/transaction/getTotalTransactions/{transType}")
+
+    @GetMapping("getTotalTransactions/{transType}")
+    @Retryable(value = {MongoException.class}, maxAttempts = 2, backoff = @Backoff(delay = 1000))
     public ResponseEntity<String> getTotalTransactions(@PathVariable String transType){
         String responseMessage = "Total "+ transType + " : "+ customerTransactionService.getTotalTransactions(transType);
          return ResponseEntity.of(Optional.of(responseMessage));
     }
-    @GetMapping("/transaction/getTotalTransactionsByDate/{date}")
+    @GetMapping("getTotalTransactionsByDate/{date}")
+    @Retryable(value = {MongoException.class}, maxAttempts = 2, backoff = @Backoff(delay = 1000))
     public ResponseEntity<Map<String, BigDecimal>> getTotalTransactionsByDate(@PathVariable String date){
         Map<String, BigDecimal> data  =customerTransactionService.getTotalTransactionsByDate(LocalDate.parse(date));
         return ResponseEntity.of(Optional.of(data));
+    }
+
+    public ResponseEntity<String> createNewTransactionFallBack(@PathVariable String email, @RequestBody @Valid TransactionData transactionData, Exception exception){
+        throw new TransactionServiceException("Customer Service Not Available", exception);
     }
 
 }
